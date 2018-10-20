6c53347827b8a0504ece0b41136fb553f734b3c3
Robert Chen <robertchen234@gmail.com>
2018-10-20 21:47:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2 
     array.join(" and ")
   else
-    array[-2] = "and " + array[-2]
+    array.last = "and " + array.last
     array.join(", ")
   end
 end
